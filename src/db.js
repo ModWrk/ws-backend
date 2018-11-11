7254ddf29fba54b12f6b146709e1d13a78dbe816
ModWrk <erik@modwrk.com>
2018-11-11 13:20:36 -0700

    GraphQL Yoga Server working

--- src/db.js
+++ src/db.js
@@ -1,11 +1,11 @@
-// This file connects to the Prisma DB and allows to access it with JS
+// This file connects to the Prisma DB and allow access to it with JS
 
 const { Prisma } = require('prisma-binding');
 
 const db = new Prisma({
     typeDefs:'src/generated/prisma.graphql',
-    endpoint:'prisma.env.PRISMA_ENDPOINT',
-    secret:'prisma.env.PRISMA_SECRET',
+    endpoint:process.env.PRISMA_ENDPOINT,
+    secret:process.env.PRISMA_SECRET,
     debug: false,
 });
 
